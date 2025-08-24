02db27907db2bef57d24283585492150559f3fbc
tharinduWageesha <tharinduwageesha@gmail.com>
2025-08-24 09:13:55 +0530

    Updated Nav Bars

---
+++ Admin/adminhomepage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Outer Web - Start Your Career</title>
+  <link rel="stylesheet" href="adminhomepage.css">
+</head>
+<body>
+  <!-- Header -->
+  <header>
+    <nav>
+      <div class="logo">
+        <img src="../assets/images/logo2.png " height="50" width="50">
+        <div>
+          <strong>CareerConnect</strong>
+          <div class="tagline">Connecting Careers, Building Futures.</div>
+        </div>
+      </div>
+      
+      <ul class="nav-links">
+        <li><a href="#about">Home</a></li>
+        <li><a href="#vacancies">Manage Employees</a></li>
+        <li><a href="#values">Manage Companies</a></li>
+        <li><a href="#people">Help</a></li>
+        <li><a href="#contact">My Account</a></li>
+        <button onclick="window.location.href='../login.php'">Log Out</button>
+      </ul>
+
+
+    </nav>
+  </header>
+
+  </body>
+</html>
